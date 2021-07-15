5c40c68ac5d099e52ec42af4f4c2c78b44a5ac2c
SAP Consultores <72764812+ConsultoresSAP@users.noreply.github.com>
2021-07-15 17:50:49 -0600

    Master Data Objects

---
+++ ConexionSAP/ConexionSAP/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConexionSAP
+{
+    public partial class Form1 : Form
+    {
+        private SAP sap;
+        public Form1()
+        {
+            InitializeComponent();
+            sap = new SAP();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sap.Conectar();
+                if (sap.Error != "")
+                {
+                    MessageBox.Show("Error: "+sap.Error);
+                }else
+                {
+                    MessageBox.Show("Conectados a " + sap.CName);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.sap.Desconectar();
+                if (this.sap.Error != "")
+                {
+                    MessageBox.Show("Error " + this.sap.Error);
+                }else
+                {
+                    MessageBox.Show("Desconectados");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.sap.CrearSN();
+                if (this.sap.Error != "")
+                {
+                    MessageBox.Show("Error: " + this.sap.Error);
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.sap.EditarSN("CL01");
+                if (this.sap.Error != "")
+                {
+                    MessageBox.Show("Error: " + this.sap.Error);
+                }else
+                {
+                    MessageBox.Show("Actualizado.");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.sap.EDITContactosSN("CL03", 2);
+                if (this.sap.Error != "")
+                {
+                    MessageBox.Show("Error: " + this.sap.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Actualizado.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ConexionSAP/ConexionSAP/SAP.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAPbobsCOM;
+
+namespace ConexionSAP
+{
+    class SAP
+    {
+        private Company oCom;
+        public string Error="";
+        public string CName = "";
+
+        public SAP()
+        {
+            this.oCom = new Company();
+        }
+
+
+        public void Conectar()
+        {
+            try
+            {
+                this.oCom.Server = "LABORATORIO";
+                this.oCom.DbServerType = BoDataServerTypes.dst_MSSQL2014;
+                this.oCom.UserName = "manager";
+                this.oCom.Password = "manager";
+                this.oCom.CompanyDB = "SBODemoGT";
+
+                //this.oCom.DbUserName = "sa";
+                //this.oCom.DbPassword = "SAPB1Admin1";
+
+                if (!this.oCom.Connected)
+                {
+                    int ErrorCode = this.oCom.Connect();
+
+                    if (ErrorCode != 0)
+                    {
+                        this.Error = this.oCom.GetLastErrorDescription() + "( " + ErrorCode.ToString() + ")";
+                    }
+                    else
+                    {
+                        this.CName = this.oCom.CompanyName;
+                    }
+                }
+                else
+                {
+                    this.Error = "Ya esta conectado";
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                this.Error = e.Message;
+            }
+            finally
+            {
+
+            }
+        }
+
+
+
+        public void Desconectar()
+        {
+            try
+            {
+                this.Error = "";
+                if (this.oCom != null)
+                {
+                    this.oCom.Disconnect();
+                }
+            }catch(Exception e)
+            {
+                this.Error = e.Message;
+            }
+            finally
+            {
+                if (this.oCom != null)
+                {
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(this.oCom);
+                    this.oCom = null;
+                }
+                
+            }
+        }
+
+
+        public void CrearSN()
+        {
+            SAPbobsCOM.BusinessPartners oSN = null;
+            try
+            {
+                this.Error = "";
+                oSN = (SAPbobsCOM.BusinessPartners)this.oCom.GetBusinessObject(BoObjectTypes.oBusinessPartners);
+                oSN.CardCode = "CL03";
+                oSN.CardName = "Cliente TEST";
+                oSN.CardType = BoCardTypes.cCustomer;
+                oSN.FederalTaxID = "123456789023";
+
+                if (oSN.Add() != 0)
+                {
+                    this.Error = this.oCom.GetLastErrorDescription();
+                }
+
+            }catch(Exception e)
+            {
+                this.Error = e.Message;
+            }
+            finally
+            {
+                if (oSN != null)
+                {
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(oSN);
+                    oSN = null;
+                }
+            }
+        }
+
+
+        public void EditarSN(string CardCode)
+        {
+            SAPbobsCOM.BusinessPartners oSN = null;
+            try
+            {
+                this.Error = "";
+                oSN = (SAPbobsCOM.BusinessPartners)this.oCom.GetBusinessObject(BoObjectTypes.oBusinessPartners);
+
+                if (oSN.GetByKey(CardCode))
+                {
+                    oSN.EmailAddress = "TEST@gmail.com";
+
+                    if (oSN.Update() != 0)
+                    {
+                        this.Error = this.oCom.GetLastErrorDescription();
+                    }
+                }
+                else
+                {
+                    this.Error = "SN no existe";
+                }
+
+            }catch(Exception e)
+            {
+                this.Error = e.Message;
+            }
+            finally
+            {
+                if (oSN != null)
+                {
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(oSN);
+                    oSN = null;
+                }
+            }
+        }
+
+
+        public void ADDContactosSN(string CardCode)
+        {
+            SAPbobsCOM.BusinessPartners oSN = null;
+            try
+            {
+                this.Error = "";
+                oSN = (SAPbobsCOM.BusinessPartners)this.oCom.GetBusinessObject(BoObjectTypes.oBusinessPartners);
+
+                if (oSN.GetByKey(CardCode))
+                {
+
+                    if (oSN.ContactEmployees.Count > 1)
+                    {
+                        oSN.ContactEmployees.Add();
+                    }else
+                    {
+                        if (oSN.ContactEmployees.Name != "")
+                        {
+                            oSN.ContactEmployees.Add();
+                        }
+                    }
+                    
+                    oSN.ContactEmployees.Name = "PRIMERO";
+                    oSN.ContactEmployees.FirstName = "Pedro2";
+                    oSN.ContactEmployees.MiddleName = "Juan2";
+                    oSN.ContactEmployees.LastName = "Perez2";
+                    oSN.ContactEmployees.Title = "Sr..";
+                    //oSN.ContactEmployees.Address = "Test";
+                    //oSN.ContactEmployees.Phone1 = "89451335";
+                    //oSN.ContactEmployees.MobilePhone = "56784512";
+                    //oSN.ContactEmployees.E_Mail = "Test@gmail.com";
+
+                    if (oSN.Update() != 0)
+                    {
+                        this.Error = this.oCom.GetLastErrorDescription();
+                    }
+                }
+                else
+                {
+                    this.Error = "SN no existe";
+                }
+
+            }
+            catch (Exception e)
+            {
+                this.Error = e.Message;
+            }
+            finally
+            {
+                if (oSN != null)
+                {
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(oSN);
+                    oSN = null;
+                }
+            }
+        }
+
+
+        public void EDITContactosSN(string CardCode,int Line)
+        {
+            SAPbobsCOM.BusinessPartners oSN = null;
+            try
+            {
+                this.Error = "";
+                oSN = (SAPbobsCOM.BusinessPartners)this.oCom.GetBusinessObject(BoObjectTypes.oBusinessPartners);
+
+                if (oSN.GetByKey(CardCode))
+                {
+
+
+                    oSN.ContactEmployees.SetCurrentLine(Line);
+                    
+                    oSN.ContactEmployees.FirstName = "";
+                    oSN.ContactEmployees.MiddleName = "";
+                    oSN.ContactEmployees.LastName = "";
+                    oSN.ContactEmployees.Title = "";
+                    //oSN.ContactEmployees.Address = "Test";
+                    //oSN.ContactEmployees.Phone1 = "89451335";
+                    //oSN.ContactEmployees.MobilePhone = "56784512";
+                    //oSN.ContactEmployees.E_Mail = "Test@gmail.com";
+
+                    if (oSN.Update() != 0)
+                    {
+                        this.Error = this.oCom.GetLastErrorDescription();
+                    }
+                }
+                else
+                {
+                    this.Error = "SN no existe";
+                }
+
+            }
+            catch (Exception e)
+            {
+                this.Error = e.Message;
+            }
+            finally
+            {
+                if (oSN != null)
+                {
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(oSN);
+                    oSN = null;
+                }
+            }
+        }
+
+
+
+    }
+}
